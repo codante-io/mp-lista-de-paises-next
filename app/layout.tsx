6104d9635292e5eed5de853ead48d92fb6981748
Nathan Mota <nathansmota@gmail.com>
2023-10-08 12:56:13 -0300

    Inital Commit
    Definindo a Estrutura Padrão de Projeto

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Nunito_Sans } from 'next/font/google'
+import Image from 'next/image'
+
+const nunitoSans = Nunito_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Lista de Países',
+  description: 'Uma lista de países criada com Next 13',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={nunitoSans.className}>
+        <main className='bg-gray-100 min-h-screen'>
+          <nav className='w-full bg-white h-16 flex items-center justify-center'>
+            <section className='container flex items-center gap-3'>
+              <Image src="Logo.svg" alt="Logo" width={48} height={48} />
+              <h1 className='font-bold text-2xl'>Países do mundo</h1>
+            </section>
+          </nav>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
