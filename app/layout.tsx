f1f74beeb5306366d997a9a6f8854f31bba9500e
Nathan Mota <nathansmota@gmail.com>
2023-10-09 14:44:36 -0300

    Fetch de dados no Server Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunitoSans.className}>
-        <main className='bg-gray-100 min-h-screen'>
+        <main className='bg-gray-100 min-h-screen flex flex-col items-center'>
           <nav className='w-full bg-white h-16 flex items-center justify-center'>
             <section className='container flex items-center gap-3'>
               <Image src="Logo.svg" alt="Logo" width={48} height={48} />
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <h1>teste</h1>;
+import { get } from "http";
+
+type Country = {
+  name: {
+    common: string;
+  }
+}
+
+//fetch de dados - next 13 lida com isso (cache)
+async function getCountries(): Promise<Country[]> {
+  const response = await fetch('https://restcountries.com/v3.1/all');
+  return response.json();
+}
+
+//componente funciona pq está rodando no servidor
+export default async function Home() {
+  const countries = await getCountries();
+
+  return (
+    <section className="flex w-full container">
+      {countries.map((country) => (
+        <h1 key={country.name.common}>{country.name.common}</h1>
+      ))}
+    </section>
+  );
 }
