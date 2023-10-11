24c6effc836fec6d7fa38d580968c6432cf8f2ae
Nathan Mota <nathansmota@gmail.com>
2023-10-10 23:10:53 -0300

    criando o card do país

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
 import { get } from "http";
+import Image from 'next/image';
 
 type Country = {
+
   name: {
     common: string;
-  }
-}
+  };
+
+  translations: {
+    por: {
+      common: string;
+    };
+  };
+
+  flags: {
+    svg: string;
+    alt: string;
+  };
+
+};
 
 //fetch de dados - next 13 lida com isso (cache)
 async function getCountries(): Promise<Country[]> {
@@ -17,9 +31,19 @@ export default async function Home() {
   const countries = await getCountries();
 
   return (
-    <section className="flex w-full container">
+    <section className="grid grid-cols-5 w-full container gap-2 mt-16">
       {countries.map((country) => (
-        <h1 key={country.name.common}>{country.name.common}</h1>
+
+        <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl
+         hover:border-indigo-200 transition-all hover:shadow-xl" key={country.name.common}>
+
+          <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
+            <Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover" />
+          </div>
+
+          <h1 className="font-bold text-xl text-center mt-1">{country.translations.por.common}</h1>
+        </article>
+
       ))}
     </section>
   );
