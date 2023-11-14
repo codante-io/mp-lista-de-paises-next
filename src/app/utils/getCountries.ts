8012979fe9097f1d4a65d4ac55198ca9fa7b4cc4
Lord Ghapa <felipef32@hotmail.com>
2023-11-14 05:48:04 -0300

    🚀 Deploy: deploy

--- src/app/utils/getCountries.ts
+++ src/app/utils/getCountries.ts
@@ -1,6 +1,6 @@
 import { Country } from "@/types";
 
 export async function getCountries(): Promise<Country[]> {
-  const res = await fetch("https://restcountries.com/v3.1/all");
+  const res = await fetch("https://restcountries.com/v3.1/all" );
   return res.json();
 }
