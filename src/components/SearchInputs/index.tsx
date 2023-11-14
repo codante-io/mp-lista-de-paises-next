0f62579b03bf60fb3782622e5d4e7ce638601bae
Lord Ghapa <felipef32@hotmail.com>
2023-11-14 00:37:01 -0300

    🚀 Deploy: temp deploy testing

--- src/components/SearchInputs/index.tsx
+++ src/components/SearchInputs/index.tsx
@@ -1,48 +1,48 @@
-export default function filterInputs() {
-  return (
-    <div className="mt-3 flex items-center gap-5">
-      <span>Filtrar por:</span>
-      <form
-        className="flex items-center gap-5"
-        onSubmit={(e) => {
-          {
-            e.preventDefault();
-            handleFilter();
-          }
-        }}
-      >
-        <label htmlFor="name">Nome:</label>
-        <input
-          id="name"
-          type="search"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="bg-gray-100 p-2 text-black"
-        />
-        <label htmlFor="language">Idioma:</label>
-        <input
-          className="p-2"
-          list="languages"
-          id="language"
-          value={searchByLanguage}
-          onChange={(e) => setSearchByLanguage(e.target.value)}
-        />
-        <datalist id="languages">
-          <OptionsLanguage />
-        </datalist>
-        <label htmlFor="continent">Continente:</label>
-        <select
-          id="continent"
-          className="p-2"
-          value={searchByContinent}
-          onChange={(e) => setSearchByContinent(e.target.value)}
-        >
-          <option value="">Select an Option</option>
-          <OptionsContinent />
-        </select>
-        <input type="submit" value="Submit" onClick={() => "s"} />
-        <input type="reset" value="Reset" onClick={resetSearch} />
-      </form>
-    </div>
-  );
-}
+// export default function filterInputs() {
+//   return (
+//     <div className="mt-3 flex items-center gap-5">
+//       <span>Filtrar por:</span>
+//       <form
+//         className="flex items-center gap-5"
+//         onSubmit={(e) => {
+//           {
+//             e.preventDefault();
+//             handleFilter();
+//           }
+//         }}
+//       >
+//         <label htmlFor="name">Nome:</label>
+//         <input
+//           id="name"
+//           type="search"
+//           value={search}
+//           onChange={(e) => setSearch(e.target.value)}
+//           className="bg-gray-100 p-2 text-black"
+//         />
+//         <label htmlFor="language">Idioma:</label>
+//         <input
+//           className="p-2"
+//           list="languages"
+//           id="language"
+//           value={searchByLanguage}
+//           onChange={(e) => setSearchByLanguage(e.target.value)}
+//         />
+//         <datalist id="languages">
+//           <OptionsLanguage />
+//         </datalist>
+//         <label htmlFor="continent">Continente:</label>
+//         <select
+//           id="continent"
+//           className="p-2"
+//           value={searchByContinent}
+//           onChange={(e) => setSearchByContinent(e.target.value)}
+//         >
+//           <option value="">Select an Option</option>
+//           <OptionsContinent />
+//         </select>
+//         <input type="submit" value="Submit" onClick={() => "s"} />
+//         <input type="reset" value="Reset" onClick={resetSearch} />
+//       </form>
+//     </div>
+//   );
+// }
